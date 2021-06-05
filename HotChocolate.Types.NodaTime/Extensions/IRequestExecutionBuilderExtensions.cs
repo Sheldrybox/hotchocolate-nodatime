af80d534b31dde2084817d6cdfabb40920086dd5
Saul Nunez <saul.nunez99@gmail.com>
2021-06-04 19:10:16 -0600

    Add an extension on IRequestExecutorBuilder to allow to add NodaTime translation on AddGraphQLServer() when creating a GraphQL server for ASP .NET Core

---
+++ HotChocolate.Types.NodaTime/Extensions/IRequestExecutionBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChocolate.Types.NodaTime.Extensions
+{
+    public static class IRequestExecutionBuilderExtensions
+    {
+        public static IRequestExecutorBuilder AddNodaTime(this IRequestExecutorBuilder requestBuilder)
+        {
+            return requestBuilder
+                .AddType<DateTimeZoneType>()
+                .AddType<DurationType>()
+                .AddType<InstantType>()
+                .AddType<IsoDayOfWeekType>()
+                .AddType<LocalDateTimeType>()
+                .AddType<LocalDateType>()
+                .AddType<LocalTimeType>()
+                .AddType<OffsetDateTimeType>()
+                .AddType<OffsetDateType>()
+                .AddType<OffsetTimeType>()
+                .AddType<OffsetType>()
+                .AddType<PeriodType>()
+                .AddType<ZonedDateTimeType>();
+        }
+    }
+}
